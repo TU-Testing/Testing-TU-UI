69d7a4511b526f2f290af707c685074f988f5445
CarlosGP-TD <87336797+CarlosGP-TD@users.noreply.github.com>
2022-07-21 13:48:48 -0500

    Update

--- src/handler.js
+++ src/handler.js
@@ -2,12 +2,12 @@ module.exports.webapp = async (event) => {
     return {
         statusCode: 200,
         body: JSON.stringify(
-            value, {
+            value: {
                 message: 'Hello OneTUDN',
                 INPUT: Event,
             },
-            replacer, null,
-            space, 2
+            replacer: null,
+            space: 2
         ),
     };
 };
